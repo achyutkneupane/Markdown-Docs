f52d88ebf1cef9defffa78a9544699c8e6b5b3ec
Achyut Neupane <achyutkneupane@gmail.com>
2024-01-02 21:02:52 +0545

    feat: basic action index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+
+async function run() {
+    try {
+        const directory = core.getInput('folder');
+        const branch = core.getInput('branch');
+        const target = core.getInput('target');
+
+        await exec.exec(`${src}/build.sh -d ${directory} -b ${branch} -t ${target}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
